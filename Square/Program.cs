a38b7ff8234d9dd27d8d528960f93c61ebd876f7
lubovcha <chalovaljubov@mail.ru>
2022-09-08 02:44:35 +0300

    a

---
+++ Square/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// Console.Write("Введите число a: "); 
+// int a = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Введите число b: "); 
+// int b = Convert.ToInt32(Console.ReadLine()); 
+// или:
+// Console.Write("Введите 2 числа: "); 
+// int a = Convert.ToInt32(Console.ReadLine()); 
+// int b = Convert.ToInt32(Console.ReadLine()); 
+//
+// if (a * a == b){ //или if (Math.Sqrt(a) == b){
+//  System.Console.WriteLine($"Число {a} является квадратом числа {b}");
+// }
+// else{
+//    System.Console.WriteLine($"Число {a} не является квадратом числа {b}");
+// }
+//
+// вариант препода (проверка и числа a и числа b)
+//
+Console.Write("Введите число a: "); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число b: "); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+if (a * a == b){ //или if (Math.Sqrt(a) == b){
+   System.Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else if (b *b == a){
+    System.Console.WriteLine($"Число {b} является квадратом числа {a}");
+}
+else{
+    System.Console.WriteLine("Не является квадратом");
+}
