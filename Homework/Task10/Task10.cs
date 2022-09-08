da62bc75a2670b67559c414c05ffb0eaccfc3eda
lubovcha <chalovaljubov@mail.ru>
2022-09-08 23:08:04 +0300

    2

--- Homework/Task10/Task10.cs
+++ Homework/Task10/Task10.cs
@@ -3,8 +3,12 @@
 // 782 -> 8
 // 918 -> 1
 
-int n = new Random().Next(100, 1000); // эти две строчки просто выводят случайное число
+// int n = new Random().Next(100, 1000); // эти две строчки просто выводят случайное число
+//
+// System.Console.WriteLine(n);
+//
+// System.Console.WriteLine((n / 10) % 10);
 
-System.Console.WriteLine(n);
-
-System.Console.WriteLine((n / 10) % 10);
+// Вариант решения от препода
+int n = 531;
+System.Console.WriteLine(n / 10 % 10); // n/10 - число перестанет быть трехзначным 531 = 53, а потом мы 53 % 10, остаток от деления = 3
