5abedeb139499ee4916961646b3c66eac2129bc5
lubovcha <chalovaljubov@mail.ru>
2022-09-08 23:51:16 +0300

    2

--- Homework/Task13/Task13.cs
+++ Homework/Task13/Task13.cs
@@ -1,11 +1,18 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
 
-Console.Write("Введи число: ");
-int anyNumber = Convert.ToInt32(Console.ReadLine()); //задаем переменную anyNumber
-string anyNumberText = Convert.ToString(anyNumber); // переводим в ряд чисел
-if (anyNumberText.Length > 2){
-  Console.WriteLine($"третья цифра" + anyNumberText[2]); //тут не ставятся {} внутри совместно с []
+// Console.Write("Введи число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine()); //задаем переменную anyNumber
+// string anyNumberText = Convert.ToString(anyNumber); // переводим в ряд чисел
+// if (anyNumberText.Length > 2){
+//  Console.WriteLine($"третья цифра" + anyNumberText[2]); //тут не ставятся {} внутри совместно с []}
+// else {
+//  Console.WriteLine("третьей цифры нет");
+// }
+int n = new Random().Next(100, 10000);
+if (n < 100){                           // проверяем на трехзначность
+  System.Console.WriteLine("Третьей цифры нет");
 }
-else {
-  Console.WriteLine("третьей цифры нет");
+else{
+  int result = n / 100 % 10; // третье число
+  System.Console.WriteLine($"Третья цифра цисла {n} --> {result}");
 }
--- Homework/Task15/Program.cs
+++ Homework/Task15/Program.cs
@@ -2,12 +2,12 @@
 Console.Write("Введите номер дня недели: "); 
 int day = Convert.ToInt32(Console.ReadLine()); 
   
-if (day == 7 || day == 6){ 
+if (day == 7 || day == 6){  // if (day > 5)
     System.Console.WriteLine("Выходной"); 
 } 
-else if ((day == 2 || day == 3 || day == 4 || day == 5)){ // || это или
+else if ((day  == 1 || day == 2 || day == 3 || day == 4 || day == 5)){ // || это "или"
     System.Console.WriteLine("Будний"); 
 }
 else{ 
-    System.Console.WriteLine("Неверный день недели"); 
+    System.Console.WriteLine("Неверный день недели"); // if (day > 7 || day < 1) 
 }
